7d9032ecfe4d1ae62fb1d1477b54a1d83e571b55
Hiran Raj <hirankvlr@gmail.com>
2022-07-23 11:00:46 +0530

    content and banner section completed

--- src/Components/Content&Banner/ContentBanner.js
+++ src/Components/Content&Banner/ContentBanner.js
@@ -6,13 +6,13 @@ function ContentBanner() {
     <div className='ContentBanner'>
       <h1 className='name'>NIRMALA ACADEMY</h1>
       <h6 className='content'>Educational Consultant in Cherthala <br />
-        Opening at 8:00 AM</h6>
+        Open today until 8:00 AM</h6>
       <button type="button" id='Btn' class="btn btn-secondary">Make Appointment</button>
       <br />
       <br />
       <br />
         <div className="banner">
-          <img src={Banner} alt="" />
+          <img  src={Banner} alt="" id='img'/>
         </div>
     </div>
   )
